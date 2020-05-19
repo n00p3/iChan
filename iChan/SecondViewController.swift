53af39931e7acd6b3f52c760df7e2e71f516cc1a
Mateusz Głowski <no.name.dev.00@gmail.com>
2020-05-19 19:51:57 +0200

    Initial Commit

---
+++ iChan/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iChan
+//
+//  Created by Mateusz Głowski on 19/05/2020.
+//  Copyright © 2020 Mateusz Głowski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
