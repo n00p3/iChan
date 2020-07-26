e02ce8bda662fc3df5f453918362f150bff854a2
Mateusz Głowski <no.name.dev.00@gmail.com>
2020-07-26 18:28:43 +0200

    Implement video slider

--- iChan/VideoPlayer/Controller/VideoPlayerController.swift
+++ iChan/VideoPlayer/Controller/VideoPlayerController.swift
@@ -14,10 +14,27 @@ class VideoPlayerController : UIViewController, VLCMediaPlayerDelegate {
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var controlsView: UIVisualEffectView!
     @IBOutlet weak var sliderView: UISlider!
+    @IBOutlet weak var videoTimeLabel: UILabel!
+    @IBOutlet weak var videoRemainingTimeLabel: UILabel!
     private var player: VLCMediaListPlayer?
     @IBOutlet weak var playPauseBtn: UIButton!
 //    var videoURL = "https://i.4cdn.org/wsg/1594028421375.webm"
     var videoURL = "file:///Users/mateusz.glowski/Desktop/test_video.webm"
+    var timer: Timer?
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        timer?.invalidate()
+    }
+    
+    @objc private func sliderValueWillChange() {
+        timer?.invalidate()
+    }
+    
+    @objc private func sliderValueDidChanged() {
+        let newTime = sliderView.value * Float(player?.mediaPlayer.media.length.intValue ?? Int32(0))
+        player?.mediaPlayer.time = VLCTime(int: Int32(newTime))
+        createTimer()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +43,13 @@ class VideoPlayerController : UIViewController, VLCMediaPlayerDelegate {
         thumb = thumb?.withTintColor(.white)
         sliderView.setThumbImage(thumb, for: .normal)
         sliderView.setThumbImage(thumb, for: .highlighted)
-        
+  
+        sliderView.addTarget(self, action: #selector(sliderValueWillChange), for: .touchDown)
+        sliderView.addTarget(self, action: #selector(sliderValueDidChanged), for: .touchUpInside)
+        // TODO: Implement later.
+//        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(pinchedView(_:)))
+//        videoView.addGestureRecognizer(pinch)
+  
         controlsView.clipsToBounds = true
         controlsView.layer.cornerRadius = 10
         player = VLCMediaListPlayer(drawable: videoView)
@@ -36,8 +59,30 @@ class VideoPlayerController : UIViewController, VLCMediaPlayerDelegate {
         player?.mediaList = media
         player?.repeatMode = .repeatAllItems
         player?.play()
+        
+        createTimer()
     }
+    
+    private func createTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timer in
+            self.videoTimeLabel.text = self.player?.mediaPlayer.time.stringValue
+            self.videoRemainingTimeLabel.text = self.player?.mediaPlayer.remainingTime.stringValue
+            if self.player?.mediaPlayer.time.intValue != nil && self.player?.mediaPlayer.media?.length.intValue != nil {
+                let progress = Float(self.player!.mediaPlayer.time.intValue) / Float(self.player!.mediaPlayer.media.length.intValue)
+                self.sliderView.setValue(progress, animated: true)
+            }
+        })
+    }
+    
+//    @objc private func pinchedView(_ sender: UIPinchGestureRecognizer) {
+//        let trans = CGAffineTransform(
+//            scaleX: sender.scale,
+//            y: sender.scale)
+//        videoView.transform = trans
+//    }
   
+    
+    
     @IBAction func playPause(_ sender: Any) {
         if player!.mediaPlayer.isPlaying {
             player?.pause()
