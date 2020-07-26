a56dddacd3e04223be1ac96782fef98acbaa33a9
Mateusz Głowski <no.name.dev.00@gmail.com>
2020-07-26 17:08:08 +0200

    Fix positioning

--- iChan/VideoPlayer/Controller/VideoPlayerController.swift
+++ iChan/VideoPlayer/Controller/VideoPlayerController.swift
@@ -15,12 +15,11 @@ class VideoPlayerController : UIViewController, VLCMediaPlayerDelegate {
     @IBOutlet weak var controlsView: UIVisualEffectView!
     @IBOutlet weak var sliderView: UISlider!
     private var player: VLCMediaListPlayer?
-    var videoURL = "https://i.4cdn.org/wsg/1594028421375.webm"
+//    var videoURL = "https://i.4cdn.org/wsg/1594028421375.webm"
+    var videoURL = "file:///Users/mateusz.glowski/Desktop/test_video.webm"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        sliderView.setThumbImage(UIImage(systemName: "sliderThumb"), for: .normal)
-//        sliderView.setThumbImage(UIImage(systemName:"sliderThumb"), for: .highlighted)
         var thumb = UIImage.scale(image: UIImage(systemName: "circle.fill")!, by: 0.5)
         thumb = thumb?.withTintColor(.white)
         sliderView.setThumbImage(thumb, for: .normal)
@@ -28,13 +27,13 @@ class VideoPlayerController : UIViewController, VLCMediaPlayerDelegate {
         
         controlsView.clipsToBounds = true
         controlsView.layer.cornerRadius = 10
-//        player = VLCMediaListPlayer(drawable: videoView)
-//        player?.mediaPlayer.delegate = self
-//        let media = VLCMediaList()
-//        media.add(VLCMedia(url: URL(string: videoURL)!))
-//        player?.mediaList = media
-//        player?.repeatMode = .repeatAllItems
-//        player?.play()
+        player = VLCMediaListPlayer(drawable: videoView)
+        player?.mediaPlayer.delegate = self
+        let media = VLCMediaList()
+        media.add(VLCMedia(url: URL(string: videoURL)!))
+        player?.mediaList = media
+        player?.repeatMode = .repeatAllItems
+        player?.play()
     }
   
     func mediaPlayerStateChanged(_ aNotification: Notification!) {
