a98a88d1806ea7a7a9b8b927a5f502fa53f5723d
Mateusz Głowski <no.name.dev.0@gmail.com>
2020-09-01 19:50:14 +0200

    Add search bar to history

--- iChan/History/Controller/HistoryViewController.swift
+++ iChan/History/Controller/HistoryViewController.swift
@@ -8,36 +8,54 @@
 
 import UIKit
 
-class HistoryViewController : UITableViewController {
+class HistoryViewController : UITableViewController, UISearchBarDelegate {
+    
+    var searchController = UISearchController()
     
     var dataSource = [VisitedThread]()
+    var filteredDataSource = [VisitedThread]()
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredDataSource = dataSource.filter {
+            $0.board.lowercased().contains(searchText.lowercased()) ||
+            $0.subject.lowercased().contains(searchText.lowercased()) ||
+            $0.comment.lowercased().contains(searchText.lowercased())
+        }
+        
+        tableView.reloadData()
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        navigationItem.searchController = searchController
+        navigationItem.searchController?.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController?.searchBar.delegate = self
+            
         dataSource = VisitedThread.getFromHistory()
+        filteredDataSource = dataSource
         tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
+        return filteredDataSource.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
 
         var content = ""
-        if dataSource[indexPath.row].subject.count == 0 && dataSource[indexPath.row].comment.count == 0 {
+        if filteredDataSource[indexPath.row].subject.count == 0 && filteredDataSource[indexPath.row].comment.count == 0 {
             content = "[no comment]"
-        } else if dataSource[indexPath.row].subject.count > 0 && dataSource[indexPath.row].comment.count == 0 {
-            content = dataSource[indexPath.row].subject
-        } else if dataSource[indexPath.row].subject.count == 0 && dataSource[indexPath.row].comment.count > 0 {
-            content = dataSource[indexPath.row].comment
-        } else if dataSource[indexPath.row].subject.count > 0 && dataSource[indexPath.row].comment.count > 0 {
-            content = dataSource[indexPath.row].subject + " - " + dataSource[indexPath.row].comment
+        } else if filteredDataSource[indexPath.row].subject.count > 0 && filteredDataSource[indexPath.row].comment.count == 0 {
+            content = filteredDataSource[indexPath.row].subject
+        } else if filteredDataSource[indexPath.row].subject.count == 0 && filteredDataSource[indexPath.row].comment.count > 0 {
+            content = filteredDataSource[indexPath.row].comment
+        } else if filteredDataSource[indexPath.row].subject.count > 0 && filteredDataSource[indexPath.row].comment.count > 0 {
+            content = filteredDataSource[indexPath.row].subject + " - " + filteredDataSource[indexPath.row].comment
         }
         
-        content = dataSource[indexPath.row].board + " - " + content
+        content = filteredDataSource[indexPath.row].board + " - " + content
         let attrs: [NSAttributedString.Key : Any] = [
             .font : UIFont.systemFont(ofSize: cell.textLabel?.font.pointSize ?? 12)
         ]
@@ -50,13 +68,16 @@ class HistoryViewController : UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // TODO: Emit change on set / set value on emit.
         DataHolder.shared.currentThread = CurrentThread(
-            threadNo: dataSource[indexPath.row].threadNo,
-            board: dataSource[indexPath.row].board)
+            threadNo: filteredDataSource[indexPath.row].threadNo,
+            board: filteredDataSource[indexPath.row].board)
         
         DataHolder.shared.threadChangedEvent.emit(
             CurrentThread(
-                threadNo: dataSource[indexPath.row].threadNo,
-                board: dataSource[indexPath.row].board))
+                threadNo: filteredDataSource[indexPath.row].threadNo,
+                board: filteredDataSource[indexPath.row].board))
+        
+        searchController.isActive = false
+        
         self.tabBarController?.selectedIndex = 2
     }
 }
